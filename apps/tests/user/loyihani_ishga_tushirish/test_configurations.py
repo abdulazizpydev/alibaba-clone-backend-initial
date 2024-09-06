14ca0d7f48b058d2d5ed5fcab858cfd515f6d59a
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-09-06 09:22:47 +0500

    test changed

--- apps/tests/user/loyihani_ishga_tushirish/test_configurations.py
+++ apps/tests/user/loyihani_ishga_tushirish/test_configurations.py
@@ -16,13 +16,19 @@ def test_via_importlib():
 
 
 @pytest.mark.order(3)
+def test_via_importlib():
+    loader = importlib.util.find_spec('rest_framework_simplejwt')
+    assert loader is not None, "rest_framework_simplejwt is not installed"
+
+
+@pytest.mark.order(4)
 @pytest.mark.django_db
 def test_rest_framework():
     assert 'rest_framework' in settings.INSTALLED_APPS, "rest_framework package is not added to INSTALLED_APPS"
     assert 'DEFAULT_PERMISSION_CLASSES' in settings.REST_FRAMEWORK.keys(), "DEFAULT_PERMISSION_CLASSES not in REST_FRAMEWORK"
 
 
-@pytest.mark.order(4)
+@pytest.mark.order(5)
 @pytest.mark.django_db
 def test_rest_framework():
     assert 'rest_framework_simplejwt' in settings.INSTALLED_APPS, "rest_framework_simplejwt package is not added to INSTALLED_APPS"
