af0fada6d9f14f60ab823a4765d12bd30fedbf2f
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-09-06 08:45:54 +0500

    tests updated and added

---
+++ apps/tests/user/loyihani_ishga_tushirish/test_configurations.py
@@ -0,0 +1,40 @@
+import importlib.util
+import pytest
+from django.conf import settings
+
+
+@pytest.mark.order(1)
+def test_via_importlib():
+    loader = importlib.util.find_spec('decouple')
+    assert loader is not None, "decouple is not installed"
+
+
+@pytest.mark.order(2)
+def test_via_importlib():
+    loader = importlib.util.find_spec('rest_framework')
+    assert loader is not None, "djangorestframework is not installed"
+
+
+@pytest.mark.order(3)
+def test_via_importlib():
+    loader = importlib.util.find_spec('rest_framework_simplejwt')
+    assert loader is not None, "rest_framework_simplejwt is not installed"
+
+
+@pytest.mark.order(4)
+def test_via_importlib():
+    loader = importlib.util.find_spec('pillow')
+    assert loader is not None, "pillow is not installed"
+
+
+@pytest.mark.order(5)
+@pytest.mark.django_db
+def test_rest_framework(api_client):
+    assert 'rest_framework' in settings.INSTALLED_APPS, "rest_framework package is not added to INSTALLED_APPS"
+    assert 'DEFAULT_PERMISSION_CLASSES' in settings.REST_FRAMEWORK.keys(), "DEFAULT_PERMISSION_CLASSES not in REST_FRAMEWORK"
+
+
+@pytest.mark.order(6)
+@pytest.mark.django_db
+def test_rest_framework(api_client):
+    assert 'rest_framework_simplejwt' in settings.INSTALLED_APPS, "rest_framework_simplejwt package is not added to INSTALLED_APPS"
