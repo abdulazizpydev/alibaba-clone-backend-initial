572293d1675cc4d4e3ccf62d995a32a9c383ae9d
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-09-09 09:50:45 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -140,3 +140,14 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = 'user.User'
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
