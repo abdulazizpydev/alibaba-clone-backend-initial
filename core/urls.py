e81102924b8c4ab0136d880fd6d00c9bf0b1b1f5
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-10-02 20:33:18 +0500

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 
 def index(request):
@@ -8,6 +9,7 @@ def index(request):
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", index),
     path("health/", index),
     path("api/users/", include("user.urls", namespace="user")),
     path("api/products/", include("product.urls", namespace="product")),
@@ -17,4 +19,7 @@ urlpatterns = [
     path("api/notifications/", include("notification.urls", namespace="notification")),
     path("api/coupons/", include("coupon.urls", namespace="coupon")),
     path("api/wishlist/", include("wishlist.urls", namespace="wishlist")),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
